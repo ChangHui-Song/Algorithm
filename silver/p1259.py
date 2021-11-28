567b0fc91f5d17a36e4378f0a4971a1baed9a7ce
ChangHui-Song <sch102403@gmail.com>
2021-11-29 01:53:26 +0900

    updated 20211129

---
+++ silver/p1259.py
@@ -0,0 +1,16 @@
+import sys
+
+if __name__ == "__main__":
+	while True:
+		num = list(sys.stdin.readline().rstrip())
+		if (num == ['0']):
+			break ;
+		num_len = len(num)
+		flag = 0
+		for i in range(num_len // 2):
+			if (num[i] != num[num_len - i - 1]):
+				print('no')
+				flag = 1
+				break ;
+		if (flag == 0):
+			print('yes')
