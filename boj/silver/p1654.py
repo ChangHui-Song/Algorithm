9584587e2f6668d0afd22d5cf0556609d98ea87f
ChangHui-Song <sch102403@gmail.com>
2021-12-09 00:31:39 +0900

    updated 20211209

---
+++ boj/silver/p1654.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == "__main__":
+	k, n = map(int, sys.stdin.readline().split())
+	arr = []
+	for _ in range(k):
+		arr.append(int(sys.stdin.readline()))
+	left, right = 1, max(arr)
+	while left <= right:
+		answer = 0
+		mid = (left + right) // 2
+		for x in arr:
+			answer += x // mid
+		if answer < n:
+			right = mid - 1
+		else:
+			left = mid + 1
+	print(right)
