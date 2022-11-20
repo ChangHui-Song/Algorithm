35ac09b1fedacb906c337dde365a09965ad5d701
gigo96 <sch102403@gmail.com>
2022-11-20 21:50:52 +0900

    solve 20221120

--- js/leetcode/linked-list/mergeTwoSortedLists.js
+++ js/leetcode/linked-list/mergeTwoSortedLists.js
@@ -10,4 +10,14 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+  if (!list1 || (list2 && list1.val > list2.val)) {
+    let temp = list1;
+    list1 = list2;
+    list2 = temp;
+  }
+  if (list1) {
+    list1.next = mergeTwoLists(list1.next, list2);
+  }
+  return list1;
+};
