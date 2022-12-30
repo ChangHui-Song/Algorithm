080c32aab7a76a17c9699812e9bfdf47ea1ae2d6
gigo96 <sch102403@gmail.com>
2022-12-30 11:44:37 +0900

    solve: [ leetcode ] Serialize and Deserialize Binary Tree
    Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
    
    Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
    
    Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
    
    이진 트리를 직렬화하는 기능과 역직렬화 하는 기능을 구현하는 문제
    직렬화를 할 때는 어떠한 방법으로든 직렬화를 가능
    역직렬화는 직렬화 한 방식에 맞게 역직렬화를 진행해야함
    
    해당 문제는 bfs를 이용해 풀이했다.
    
    이진트리는 기본적으로 배열로도 구현 할 수 있기 때문에 배열로 만드는 규칙만 알고 있으면, 풀 수 있다.
    
    먼저, 이진트리를 배열로 구현 할 때 어떠한 수식으로 구성되는 지 알아야 한다.
    부모 노드가 배열의 i의 위치에 있다고 할 때, left 자식 노드는 2i, right 자식 노드는 2i + 1에 위치하게 된다.
    이 규칙에 따라 bfs로 자연스럽게 배열을 탐색하면 된다.
    
    직렬화는 간단하게 bfs를 이용해 하는데 다만, 파이썬의 경우는 None을 문자열로 치환할 수 없기 때문에 #을 이용했다.
    그 후, 배열을 문자열로 변환하면 된다.
    
    역직렬화의 경우 문자열로 구성 된 파라미터를 먼저 배열로 치환한 뒤, 하나 씩 노드로 만들어 queue에 삽입하면서 bfs를 실행한다.

---
+++ JS&TS/leetcode/tree/serialize-and-deserialize-binary-tree.ts
@@ -0,0 +1,71 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/*
+ * Encodes a tree to a single string.
+ */
+function serialize(root: TreeNode | null): string {
+  if (!root) return '';
+  const queue: (TreeNode | null)[] = [root];
+  const result: string[] = [];
+
+  while (queue.length !== 0) {
+    const node = queue.shift();
+
+    if (!node) {
+      result.push(null);
+      continue;
+    }
+    queue.push(node.left);
+    queue.push(node.right);
+    result.push(String(node.val));
+  }
+
+  return result.join(' ');
+}
+
+/*
+ * Decodes your encoded data to tree.
+ */
+function deserialize(data: string): TreeNode | null {
+  if (data.length === 0) return null;
+
+  const nodes = data.split(' ');
+  const root = new TreeNode(parseInt(nodes[0]));
+  const queue: TreeNode[] = [root];
+
+  let index = 1;
+  while (queue.length !== 0) {
+    const node = queue.shift();
+    if (!node) continue;
+
+    if (nodes[index] && nodes[index] !== 'null') {
+      node.left = new TreeNode(parseInt(nodes[index]));
+      queue.push(node.left);
+    }
+    index++;
+
+    if (nodes[index] && nodes[index] !== 'null') {
+      node.right = new TreeNode(parseInt(nodes[index]));
+      queue.push(node.right);
+    }
+    index++;
+  }
+  return root;
+}
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
+
+console.log(deserialize('1 2 3 null null 4 5')?.val);
