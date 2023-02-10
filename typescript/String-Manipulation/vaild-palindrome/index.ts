92ae2272e66c10aa958b0d98319e0a30e9724cb3
sch <sch102403@gmail.com>
2023-02-10 20:03:34 +0900

    반복문 문자열 중간까지만 가도록 수정

--- typescript/String-Manipulation/vaild-palindrome/index.ts
+++ typescript/String-Manipulation/vaild-palindrome/index.ts
@@ -2,7 +2,7 @@ export function isPalindrome(s: string): boolean {
   const lowerString = s.toLowerCase();
   const newString = lowerString.replace(/[^0-9|a-z]/gi, '');
 
-  for (let i = 0; i < newString.length; i++) {
+  for (let i = 0; i < Math.floor(newString.length / 2); i++) {
     if (newString[i] !== newString[newString.length - 1 - i]) return false;
   }
   return true;
