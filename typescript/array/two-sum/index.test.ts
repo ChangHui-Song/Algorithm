ff5944de0de07782a88e8932ec4da7b356fdcd61
sch <sch102403@gmail.com>
2023-02-16 11:40:21 +0900

    update test code

--- typescript/array/two-sum/index.test.ts
+++ typescript/array/two-sum/index.test.ts
@@ -3,14 +3,14 @@ import { twoSum } from '.';
 
 describe('two sum test describe', () => {
   test('test case 1', () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual(expect.arrayContaining([0, 1]));
   });
 
   test('test case 2', () => {
-    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+    expect(twoSum([3, 2, 4], 6)).toEqual(expect.arrayContaining([1, 2]));
   });
 
   test('test case 3', () => {
-    expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+    expect(twoSum([3, 3], 6)).toEqual(expect.arrayContaining([0, 1]));
   });
 });
