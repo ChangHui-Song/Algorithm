786b6b09c3517195a4a5c09b2f011d2953d6fd6c
sch <sch102403@gmail.com>
2023-02-23 14:18:35 +0900

    linked list 생성 하드코딩 함수로 변경

--- typescript/linked-list/merge-two-sorted-lists/index.test.ts
+++ typescript/linked-list/merge-two-sorted-lists/index.test.ts
@@ -1,18 +1,26 @@
 import { describe, expect, test } from '@jest/globals';
 import { mergeTwoLists, ListNode } from '.';
 
+const convertArrayToList = (array: number[]): ListNode | null => {
+  if (!array) return null;
+
+  const head = new ListNode(-1);
+  let prev = head;
+
+  for (let i = 0; i < array.length; i++) {
+    prev.next = new ListNode(array[i]);
+    prev = prev.next;
+  }
+
+  return head.next;
+};
+
 describe(' description', () => {
   test('example test 1', () => {
-    const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-    const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    const list1 = convertArrayToList([1, 2, 4]);
+    const list2 = convertArrayToList([1, 3, 4]);
     const output = mergeTwoLists(list1, list2);
-    const expected = new ListNode(
-      1,
-      new ListNode(
-        1,
-        new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))
-      )
-    );
+    const expected = convertArrayToList([1, 1, 2, 3, 4, 4]);
     expect(output).toEqual(expected);
   });
 
@@ -26,7 +34,7 @@ describe(' description', () => {
 
   test('example test 3', () => {
     const list1 = null;
-    const list2 = new ListNode(0);
+    const list2 = convertArrayToList([0]);
     const output = mergeTwoLists(list1, list2);
     const expected = new ListNode(0);
     expect(output).toEqual(expected);
