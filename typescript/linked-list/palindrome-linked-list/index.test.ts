85a89ae0e669b89906e083fa3f319130ce0b3e67
sch <sch102403@gmail.com>
2023-02-24 13:07:04 +0900

    refactor: ListNode utils로 위치 변경에 따른 구조 변경

--- typescript/linked-list/palindrome-linked-list/index.test.ts
+++ typescript/linked-list/palindrome-linked-list/index.test.ts
@@ -1,19 +1,17 @@
 import { describe, expect, test } from '@jest/globals';
-import { isPalindrome, ListNode } from '.';
+import { isPalindrome } from '.';
+import { ListNode, convertArrayToList } from '../utils';
 
 describe(' description', () => {
   test('example test 1', () => {
-    const input = new ListNode(
-      1,
-      new ListNode(2, new ListNode(2, new ListNode(1)))
-    );
+    const input = convertArrayToList([1, 2, 2, 1]);
     const output = isPalindrome(input);
     const expected = true;
     expect(output).toEqual(expected);
   });
 
   test('example test 2', () => {
-    const input = new ListNode(1, new ListNode(2));
+    const input = convertArrayToList([1, 2]);
     const output = isPalindrome(input);
     const expected = false;
     expect(output).toEqual(expected);
