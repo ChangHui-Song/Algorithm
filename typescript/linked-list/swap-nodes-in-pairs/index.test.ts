2daa8f8c2eb1fc97e0af58d4c1105c4449d50d6b
sch <sch102403@gmail.com>
2023-02-26 13:58:09 +0900

    solve: [ leetcode ] 24. Swap Nodes in Pairs

---
+++ typescript/linked-list/swap-nodes-in-pairs/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from '@jest/globals';
+import { swapPairs } from '.';
+import { convertArrayToList } from '../utils';
+
+describe(' description', () => {
+  test('example test 1', () => {
+    const input = convertArrayToList([1, 2, 3, 4]);
+    const output = swapPairs(input);
+    const expected = convertArrayToList([2, 1, 4, 3]);
+    expect(output).toEqual(expected);
+  });
+
+  test('example test 2', () => {
+    const input = null;
+    const output = swapPairs(input);
+    const expected = null;
+    expect(output).toEqual(expected);
+  });
+
+  test('example test 3', () => {
+    const input = convertArrayToList([1]);
+    const output = swapPairs(input);
+    const expected = convertArrayToList([1]);
+    expect(output).toEqual(expected);
+  });
+});
---
+++ typescript/linked-list/swap-nodes-in-pairs/index.ts
@@ -0,0 +1,11 @@
+import { ListNode } from '../utils';
+
+export function swapPairs(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+
+  const p = head.next;
+
+  head.next = swapPairs(p.next);
+  p.next = head;
+  return p;
+}
