c9095ddbc2f1899bfbe577f8a7c76ad97ba39f88
sch <sch102403@gmail.com>
2023-03-12 13:12:34 +0900

    맵 set 방식 수정

--- typescript/medium/top-k-frequent-elements/index.ts
+++ typescript/medium/top-k-frequent-elements/index.ts
@@ -2,9 +2,7 @@ export function topKFrequent(nums: number[], k: number): number[] {
   const freq = new Map<number, number>();
 
   for (let i = 0; i < nums.length; i++) {
-    let current = freq.get(nums[i]);
-    if (!current) current = 0;
-    freq.set(nums[i], current + 1);
+    freq.set(nums[i], freq.get(nums[i]) + 1 || 1);
   }
 
   const answer = Array.from(freq.entries()).sort((a, b) => b[1] - a[1]);
